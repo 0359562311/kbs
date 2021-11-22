ca2bc28d9d12f6ef018073ff030706c4276c0d93
Tân Kiêm <anhtanhangxom@gmail.com>
2021-11-22 22:55:47 +0700

    the last commit

--- main.py
+++
@@ -1,6 +0,0 @@
-from chefboost import Chefboost as chef
-
-if __name__ == '__main__':
-    model = chef.load_model("model.pkl")
-    res = chef.predict(model, ['thap','ngan','dai','yeu','khoe','yeu','tot','tot','kem','cham'])
-    print(res)
